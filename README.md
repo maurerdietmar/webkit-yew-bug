8030517a5eea283a7053929f013b153064a06130
Dietmar Maurer <dietmar@proxmox.com>
2023-12-29 12:02:22 +0100

    add a simple yew test app

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# webkit-yew-bug
-Reproducer for a bug running yew with webkit
+# Webkit yew bug reproducer
+
+Reproducer for a bug running yew with webkit.
+
+Inspirted by: https://github.com/jrouaix/wasm_pbm
+
+
+## To run the example:
+
+ trunk serve
+
+Then open http://locahost:8080 on you browser and press reload until you get a crash.
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+use yew::prelude::*;
+
+pub struct App {}
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {}
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        log::info!("start view");
+        let mut class = Classes::new();
+        for i in 0..100 {
+            class.push(format!("test-class-{i}"));
+        }
+        log::info!("end view");
+
+        html! {"If you reload this page, wekbit crashes sometimes."}
+    }
+}
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::Renderer::<App>::new().render();
+}
